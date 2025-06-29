2a9be880cca238e578c30bb028a020d33da18e9b
Shahzaib <heneryjeans1101@gmail.com>
2025-06-29 20:00:40 +0500

    implement stack class and its methods.

--- linked_list_stack.py
+++ linked_list_stack.py
@@ -7,8 +7,34 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def __iter__(self):
-        curNode = self.head
-        while curNode:
-            yield curNode
-            curNode = curNode.next
+class Stack:
+    def __init__(self):
+        self.LinkedList = LinkedList()
+
+    def isEmpty(self):  
+        if not self.LinkedList.head:
+            return True
+        return False
+
+    def push(self , value):
+        node = Node(value)
+        node.next = self.LinkedList.head
+        self.LinkedList.head.next = node
+
+    def pop(self):
+        if not self.LinkedList.head:
+            return 'Empty stack.'
+        else:
+            nodeValue = self.LinkedList.head.value
+            self.LinkedList.head = self.LinkedList.head.next
+            return nodeValue
+        
+    def peek(self):
+        if not self.LinkedList.head:
+            return 'Empty stack.'
+        else:
+            return self.LinkedList.head.value
+    
+    def delete(self):
+        self.LinkedList.head = None
+        
