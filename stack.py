36da90fdfae0160abaf0a81419f149601ec8fc0a
Shahzaib <heneryjeans1101@gmail.com>
2025-06-29 15:37:29 +0500

    feat : add the methods for limited python list

--- stack.py
+++ stack.py
@@ -6,3 +6,28 @@ class Stack:
         
         values = [str(x) for x in reversed(self.list)]
         return "/n".join(values)
+
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+        
+    def pop(self):
+        if self.list == []:
+            return "List is empty ."
+        else:
+            self.list.pop()
+
+    def peek(self):
+        if self.list == []:
+            return "List is empty ."
+        else:
+            return self.list[-1]
+        
+    def delete(self):
+        return self.list == None
+
+    def push(self , value):
+        self.list.append(value)
+        return "Element succesfully added ."
