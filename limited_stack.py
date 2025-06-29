0269af4f4648a5adcde75a453057dd1509f77d10
Shahzaib <heneryjeans1101@gmail.com>
2025-06-29 16:35:36 +0500

    feat : add the methods for the stack, made from limited size of python list.

--- limited_stack.py
+++ limited_stack.py
@@ -5,3 +5,35 @@ class Stack:
     def __str__(self):
         values = [str(x) for x in reversed(self.list)]
         return "n".join(values)
+
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+    
+    def isFull(self):
+        if len(self.list) == self.maxSize:
+            return True
+        else:
+           return False
+
+    def push(self , value):
+        if self.isFull():
+            return "The stack is full ."
+        else:
+            self.list.append(value)
+            return "successfully added to stack ."
+        
+
+    def pop(self):
+        if self.list == []:
+            return " Stack is already empty ."
+        else:
+            return self.list.pop()
+        
+    def peek(self):
+        if self.list == []:
+            return 'stack is empty .'
+        else:
+            return self.list[-1]
